bcecbdb2cd27a76ce233fcd39d8bacff9f24e7a8
Abdullahi Addow <jiincade@gmail.com>
2022-07-31 10:55:11 +0300

    removed late keyword from static declaration

--- lib/features/notifications/notifiers/notification_state_notifier.dart
+++ lib/features/notifications/notifiers/notification_state_notifier.dart
@@ -1,14 +1,14 @@
-import 'package:bootfi_flutter_template/features/notifications/data/models/notification_model.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-// ignore_for_file: unnecessary_late
-late final defaultNotification = NotificationModelResponse(
-  notifications: [NotificationModel(id: 'id', title: 'title', body: 'body')],
-  totalCount: 5,
-);
+import '../data/models/notification_model.dart';
 
 class NotificationStateNotifier
     extends StateNotifier<NotificationModelResponse> {
+  static final defaultNotification = NotificationModelResponse(
+    notifications: [NotificationModel(id: 'id', title: 'title', body: 'body')],
+    totalCount: 5,
+  );
+
   NotificationStateNotifier(NotificationModelResponse? notification)
       : super(notification ?? defaultNotification);
 }
