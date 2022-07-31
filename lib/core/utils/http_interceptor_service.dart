db08a451cbb057e275873b262429da22e87adb62
Abdullahi Addow <jiincade@gmail.com>
2022-07-31 16:29:53 +0300

    added retry policy.

--- lib/core/utils/http_interceptor_service.dart
+++ lib/core/utils/http_interceptor_service.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:hooks_riverpod/hooks_riverpod.dart' show Provider;
 import 'package:http_interceptor/http_interceptor.dart'
-    show InterceptedClient, InterceptorContract, RequestData, ResponseData;
+    show
+        InterceptedClient,
+        InterceptorContract,
+        RequestData,
+        ResponseData,
+        RetryPolicy;
 
-class InterceptorClientService implements InterceptorContract {
+class _InterceptorClientService implements InterceptorContract {
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
     debugPrint("request:${data.url}");
@@ -18,9 +23,35 @@ class InterceptorClientService implements InterceptorContract {
   }
 }
 
+class _ExpiredTokenRetryPolicy extends RetryPolicy {
+  @override
+  int get maxRetryAttempts => 2;
+
+  @override
+  bool shouldAttemptRetryOnException(Exception reason) {
+    debugPrint(reason.toString());
+
+    return false;
+  }
+
+  @override
+  Future<bool> shouldAttemptRetryOnResponse(ResponseData response) async {
+    if (response.statusCode == 401) {
+      debugPrint("Retrying request...");
+      //Todo
+      // Perform your token refresh here.
+
+      return true;
+    }
+
+    return false;
+  }
+}
+
 final httpProvider = Provider<InterceptedClient>((ref) {
   final client = InterceptedClient.build(
-    interceptors: [InterceptorClientService()],
+    interceptors: [_InterceptorClientService()],
+    retryPolicy: _ExpiredTokenRetryPolicy(),
   );
 
   ref.onDispose(client.close);
