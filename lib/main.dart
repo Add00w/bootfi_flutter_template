856fcbfb1195758942fd753629594a4edb5101bd
Abdullahi Addow <jiincade@gmail.com>
2022-08-02 19:17:31 +0300

    use UncontrolledProviderScope and ProviderContainer to initialize forebase and notifications.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:bootfi_flutter_template/core/config/app_config.dart';
 import 'package:flutter/foundation.dart' show kReleaseMode, debugPrint;
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart' show ProviderScope;
+import 'package:hooks_riverpod/hooks_riverpod.dart'
+    show ProviderContainer, UncontrolledProviderScope;
 import 'package:package_info_plus/package_info_plus.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 
 import './app.dart';
+import './features/notifications/notifiers/notification_state_notifier.dart';
 import 'core/constants/app_constants.dart' as app_constants;
 
 Future<void> main() async {
@@ -16,26 +19,34 @@ Future<void> main() async {
     debugPrint = (String? message, {int? wrapWidth}) {};
   }
 
+//Info for Sentry
   PackageInfo packageInfo = await PackageInfo.fromPlatform();
   String version = packageInfo.version;
   String appName = packageInfo.appName;
   app_constants.appVersion = version;
-
-  if (app_constants.sentryDns.isEmpty) {
+//Use UncontrolledProvider instead ProviderScope to initialize
+// some services at start.
+  final container = ProviderContainer();
+  // read the services you want to be pre-initialized
+  container.read(notificationsStateNotifierProvider);
+  final config = await container.read(configurationsProvider.future);
+  if (config.sentryDns.isEmpty) {
     runApp(
-      const ProviderScope(
-        child: App(),
+      UncontrolledProviderScope(
+        container: container,
+        child: const App(),
       ),
     );
   } else {
     await SentryFlutter.init(
       (options) => options
-        ..dsn = app_constants.sentryDns
+        ..dsn = config.sentryDns
         ..release = '$appName@$version'
-        ..environment = app_constants.envName,
+        ..environment = config.envName,
       appRunner: () => runApp(
-        const ProviderScope(
-          child: App(),
+        UncontrolledProviderScope(
+          container: container,
+          child: const App(),
         ),
       ),
     );
