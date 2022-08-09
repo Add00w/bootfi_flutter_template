59d4f494c047213e048767cfd275ca3f99cf7f45
Abdullahi Addow <jiincade@gmail.com>
2022-08-09 17:30:06 +0300

    removed debugPrint

--- lib/features/settings/notifiers/settings_state_notifier.dart
+++ lib/features/settings/notifiers/settings_state_notifier.dart
@@ -1,17 +1,14 @@
 import 'dart:ui' show Locale;
 
-import 'package:flutter/foundation.dart' show debugPrint;
 import 'package:hooks_riverpod/hooks_riverpod.dart' show StateProvider;
 
-import '../../../core/utils/helpers.dart';
+import '../../../core/core.dart';
 
 final currentLocaleProvider = StateProvider<Locale?>((ref) {
-  debugPrint('in current locale');
   return null;
 });
 final selectedLocaleProvider = StateProvider<Locale?>((ref) {
   final currentLocale = ref.watch(currentLocaleProvider);
-  debugPrint('in selected locale');
   return currentLocale;
 });
 final supportedLanguagesProvider = StateProvider<Iterable<String>>((ref) {
