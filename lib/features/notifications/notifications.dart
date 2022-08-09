4493bf8c327d8ddbf50cb15706e86bfaf4db796c
Abdullahi Addow <jiincade@gmail.com>
2022-08-09 17:29:40 +0300

    and removed them the files that exports.

--- lib/features/notifications/notifications.dart
+++ lib/features/notifications/notifications.dart
@@ -4,4 +4,3 @@ export './data/repositories/notifications_repo.dart';
 export './notifiers/notifiers.dart';
 export './presentaion/views/views.dart';
 export './presentaion/widgets/widgets.dart';
-export './providers.dart';
--- lib/features/settings/settings.dart
+++ lib/features/settings/settings.dart
@@ -1,4 +1,3 @@
 export './notifiers/notifiers.dart';
 export './presentaion/views/views.dart';
 export './presentaion/widgets/widgets.dart';
-export './providers.dart';
