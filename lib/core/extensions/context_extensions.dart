ae695fee3ad20254fcf65afccb3403b2d80d1a50
Abdullahi Addow <jiincade@gmail.com>
2022-08-22 11:31:17 +0300

    create extensions.

--- lib/core/extensions/context_extensions.dart
+++ lib/core/extensions/context_extensions.dart
@@ -7,4 +7,15 @@ extension ContextExtensions on BuildContext {
   Size get screenSize => MediaQuery.of(this).size;
   ThemeData get theme => Theme.of(this);
   TextTheme get textTheme => Theme.of(this).textTheme;
+  // Use named or Navigator 2
+  //So we don't need these extensions
+  void push(Widget page) {
+    Navigator.of(this).push(
+      MaterialPageRoute(
+        builder: (context) => page,
+      ),
+    );
+  }
+
+  void pop() => Navigator.pop(this);
 }
